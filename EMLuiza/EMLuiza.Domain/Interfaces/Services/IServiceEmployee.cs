537200015ffb44e7b3ad11d875bd7f1558df0ed5
Marcelo Souza <chariondm@gmail.com>
2018-07-25 11:55:12 -0300

    Create Interface IServiceEmployee

---
+++ EMLuiza/EMLuiza.Domain/Interfaces/Services/IServiceEmployee.cs
@@ -0,0 +1,15 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Domain.Interfaces.Services.Base;
+using System.Linq;
+
+namespace EMLuiza.Domain.Interfaces.Services
+{
+    public interface IServiceEmployee : IServiceBase
+    {
+        void Create(Employee employee);
+
+        IQueryable<Employee> List();
+
+        void Remove(Employee employee);
+    }
+}
