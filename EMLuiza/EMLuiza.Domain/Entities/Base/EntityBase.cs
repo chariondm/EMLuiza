862cbf3e18dce9e4cf40d91bec6393b411bab319
Marcelo Souza <marcelo.souza@globalhitss.com.br>
2018-07-25 01:08:20 -0300

    Create EntityBase

---
+++ EMLuiza/EMLuiza.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EMLuiza.Domain.Entities.Base
+{
+    public abstract class EntityBase
+    {
+        protected EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
