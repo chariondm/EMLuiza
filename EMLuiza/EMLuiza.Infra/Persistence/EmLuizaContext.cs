bde0a004522787f6ab7d109f6aee40db0cc21f24
Marcelo Souza <chariondm@gmail.com>
2018-07-25 18:53:06 -0300

    Create context of Database

---
+++ EMLuiza/EMLuiza.Infra/Persistence/EmLuizaContext.cs
@@ -0,0 +1,31 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Infra.Persistence.Map;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EMLuiza.Infra.Persistence
+{
+    public class EMLuizaContext : DbContext
+    {
+        public EMLuizaContext() : base("dbEMLuiza")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public IDbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.Configurations.Add(new EmployeeMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
