c4e7c833ae62a294325070eb05975ad3ae1519b0
Marcelo Souza <chariondm@gmail.com>
2018-07-25 01:36:00 -0300

    Include constructor and valueObjects in Employee entity.

--- EMLuiza/EMLuiza.Domain/Entities/Employee.cs
+++ EMLuiza/EMLuiza.Domain/Entities/Employee.cs
@@ -1,12 +1,22 @@
 ﻿using EMLuiza.Domain.Entities.Base;
+using EMLuiza.Domain.ValueObjects;
 
 namespace EMLuiza.Domain.Entities
 {
     class Employee : EntityBase
     {
-        public string Name { get; private set; }
+        public Employee(Name name, Email email, string department)
+        {
+            Name = name;
+            Email = email;
+            Department = department;
+            
+            AddNotifications(name, email);
+        }
 
-        public string Email { get; private set; }
+        public Name Name { get; private set; }
+
+        public Email Email { get; private set; }
 
         public string Department { get; private set; }
     }
