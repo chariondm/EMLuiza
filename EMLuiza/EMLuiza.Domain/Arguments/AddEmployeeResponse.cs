bb69ae28b542e79eb0381a22b230e2b092dc6310
Marcelo Souza <chariondm@gmail.com>
2018-07-25 14:29:55 -0300

    Create argument AddEmployeeResponse.

---
+++ EMLuiza/EMLuiza.Domain/Arguments/AddEmployeeResponse.cs
@@ -0,0 +1,21 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Domain.Interfaces.Arguments;
+using System;
+
+namespace EMLuiza.Domain.Arguments
+{
+    public class AddEmployeeResponse : IResponse
+    {
+        public Guid Id { get; private set; }
+        public string Message { get; private set; }
+
+        public static explicit operator AddEmployeeResponse(Employee employee)
+        {
+            return new AddEmployeeResponse()
+            {
+                Id = employee.Id,
+                Message = "Operação realizada com sucesso!"
+            };
+        }
+    }
+}
