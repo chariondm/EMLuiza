b88275090ee454e5267bc096d926306af145b9be
Marcelo Souza <chariondm@gmail.com>
2018-07-25 01:28:26 -0300

    Create ValueObject Name

---
+++ EMLuiza/EMLuiza.Domain/ValueObjects/Name.cs
@@ -0,0 +1,21 @@
+﻿using EMLuiza.Domain.ValueObjects.Base;
+using prmToolkit.NotificationPattern;
+
+namespace EMLuiza.Domain.ValueObjects
+{
+    public class Name : ValueObjectBase
+    {
+        public Name(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            new AddNotifications<Name>(this)
+                .IfNullOrInvalidLength(FirstName, 3, 50, "Name.FirstName", "Invalid FirstName")
+                .IfNullOrInvalidLength(LastName, 3, 50, "Name.LastName", "Invalid LastName");
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
