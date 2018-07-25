a1a106cfa998e6f837ec1ca4d149a3f849c68be9
Marcelo Souza <chariondm@gmail.com>
2018-07-25 14:41:57 -0300

    Create argument RemoveEmployeeResponse.

---
+++ EMLuiza/EMLuiza.Domain/Arguments/RemoveEmployeeResponse.cs
@@ -0,0 +1,21 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Domain.Interfaces.Arguments;
+using System;
+
+namespace EMLuiza.Domain.Arguments
+{
+    public class RemoveEmployeeResponse : IResponse
+    {
+        public Guid Id { get; private set; }
+        public string Message { get; private set; }
+
+        public static explicit operator RemoveEmployeeResponse(Employee employee)
+        {
+            return new RemoveEmployeeResponse()
+            {
+                Id = employee.Id,
+                Message = "Registro removido com sucesso!"
+            };
+        }
+    }
+}
