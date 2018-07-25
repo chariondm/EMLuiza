a41f4b3351a9c0ebe337c6a7caffd0f4af1040fa
Marcelo Souza <chariondm@gmail.com>
2018-07-25 14:35:12 -0300

    Create Service ServiceEmployee.

---
+++ EMLuiza/EMLuiza.Domain/Services/ServiceEmployee.cs
@@ -0,0 +1,26 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Domain.Interfaces.Services;
+using prmToolkit.NotificationPattern;
+using System;
+using System.Linq;
+
+namespace EMLuiza.Domain.Services
+{
+    public class ServiceEmployee : Notifiable, IServiceEmployee
+    {
+        public void Create(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Employee> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
