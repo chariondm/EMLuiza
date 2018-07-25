8086659daaadf7f481dce78ae30d1c70ee02d809
Marcelo Souza <marcelo.souza@globalhitss.com.br>
2018-07-25 01:12:10 -0300

    Create entity employee.

---
+++ EMLuiza/EMLuiza.Domain/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using EMLuiza.Domain.Entities.Base;
+
+namespace EMLuiza.Domain.Entities
+{
+    class Employee : EntityBase
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Department { get; private set; }
+    }
+}
