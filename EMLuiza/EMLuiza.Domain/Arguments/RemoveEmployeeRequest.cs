ea7e248e6eb3da7606e2741c45c9a9dbfe1c7430
Marcelo Souza <chariondm@gmail.com>
2018-07-25 14:39:39 -0300

    Create argument RemoveEmployeeRequest.

---
+++ EMLuiza/EMLuiza.Domain/Arguments/RemoveEmployeeRequest.cs
@@ -0,0 +1,15 @@
+﻿using EMLuiza.Domain.Interfaces.Arguments;
+using System;
+
+namespace EMLuiza.Domain.Arguments
+{
+    public class RemoveEmployeeRequest : IRequest
+    {
+        public RemoveEmployeeRequest(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
