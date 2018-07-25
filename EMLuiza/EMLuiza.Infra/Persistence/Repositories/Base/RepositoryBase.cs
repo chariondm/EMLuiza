68f39d1d98ad1eef43e597fb5660f37bf58acd4a
Marcelo Souza <chariondm@gmail.com>
2018-07-25 19:21:44 -0300

    Create Repository Base.

---
+++ EMLuiza/EMLuiza.Infra/Persistence/Repositories/Base/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using EMLuiza.Domain.Entities.Base;
+using EMLuiza.Domain.Interfaces.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace EMLuiza.Infra.Persistence.Repositories.Base
+{
+    public class RepositoryBase<TEntity, TId> : IRepositoryBase<TEntity, TId>
+        where TEntity : EntityBase
+        where TId : struct
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity Create(TEntity entity)
+        {
+            return _context.Set<TEntity>().Add(entity);
+        }
+
+        public IQueryable<TEntity> List(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (includeProperties.Any())
+            {
+                return Include(_context.Set<TEntity>(), includeProperties);
+            }
+
+            return query;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        private IQueryable<TEntity> Include(IQueryable<TEntity> query, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            foreach (var property in includeProperties)
+            {
+                query = query.Include(property);
+            }
+
+            return query;
+        }
+    }
+}
