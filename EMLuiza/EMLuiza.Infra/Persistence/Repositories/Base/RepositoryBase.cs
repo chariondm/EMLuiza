11f915f97b9cc881f9125f65c7e47242c5ca5df2
Marcelo Souza <chariondm@gmail.com>
2018-07-25 19:28:12 -0300

    Create Employee Repository.

--- EMLuiza/EMLuiza.Infra/Persistence/Repositories/Base/RepositoryBase.cs
+++ EMLuiza/EMLuiza.Infra/Persistence/Repositories/Base/RepositoryBase.cs
@@ -1,13 +1,9 @@
 ﻿using EMLuiza.Domain.Entities.Base;
 using EMLuiza.Domain.Interfaces.Repositories.Base;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-
-using System.Threading.Tasks;
 
 namespace EMLuiza.Infra.Persistence.Repositories.Base
 {
---
+++ EMLuiza/EMLuiza.Infra/Persistence/Repositories/RepositoryEmployee.cs
@@ -0,0 +1,17 @@
+﻿using EMLuiza.Domain.Entities;
+using EMLuiza.Domain.Interfaces.Repositories;
+using EMLuiza.Infra.Persistence.Repositories.Base;
+using System;
+
+namespace EMLuiza.Infra.Persistence.Repositories
+{
+    public class RepositoryEmployee : RepositoryBase<Employee, Guid>, IRepositoryEmployee
+    {
+        protected readonly EMLuizaContext _context;
+
+        public RepositoryEmployee(EMLuizaContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
