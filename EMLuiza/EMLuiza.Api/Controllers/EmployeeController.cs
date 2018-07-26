d8023d0c9b87b2d70d5c3931cb69a745a1a1d54e
Marcelo Souza <chariondm@gmail.com>
2018-07-25 21:42:42 -0300

    Create Employee Controller.

---
+++ EMLuiza/EMLuiza.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using EMLuiza.Api.Controllers.Base;
+using EMLuiza.Domain.Arguments;
+using EMLuiza.Domain.Interfaces.Services;
+using EMLuiza.Infra.Transactions;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EMLuiza.Api.Controllers
+{
+    [RoutePrefix("employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IServiceEmployee _serviceEmployee;
+
+        public EmployeeController(IUnitOfWork unitOfWork, IServiceEmployee serviceEmployee) : base(unitOfWork)
+        {
+            _serviceEmployee = serviceEmployee;
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create(AddEmployeeRequest request)
+        {
+            try
+            {
+                var response = _serviceEmployee.Create(request);
+
+                return await ResponseAsync(response, _serviceEmployee);
+            }
+            catch (Exception ex)
+            {
+                return await ResponseExceptionAsync(ex);
+            }
+        }
+    }
+}
