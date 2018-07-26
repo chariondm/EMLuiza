e54545356f4a6316ffbaf709e0ab109409b0104b
Marcelo Souza <chariondm@gmail.com>
2018-07-25 21:27:07 -0300

    Create Startup project.

---
+++ EMLuiza/EMLuiza.Api/Startup.cs
@@ -0,0 +1,61 @@
+﻿using EMLuiza.IoC.Unity;
+using Microsoft.AspNet.WebApi.Extensions.Compression.Server;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Net.Http.Extensions.Compression.Core.Compressors;
+using System.Web.Http;
+
+namespace EMLuiza.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Configure Dependency Injection
+            var container = new UnityContainer();
+            DependencyResolver.Resolve(container);
+            config.DependencyResolver = new UnityResolver(container);
+
+            ConfigureWebApi(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+
+            formatters.Remove(formatters.XmlFormatter);
+
+            config.MessageHandlers.Insert(0, new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
+
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+            Register(config);
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
